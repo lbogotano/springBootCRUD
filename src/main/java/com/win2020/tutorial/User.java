4c01933093fb3238fdc1a4a34cfd233bfd7fae40
Luis Gonzalez <lgonzalez.us1@gmail.com>
2020-06-22 21:23:57 -0400

    commit with corrections

---
+++ src/main/java/com/win2020/tutorial/User.java
@@ -0,0 +1,62 @@
+package com.win2020.tutorial;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    // Constructors
+    public User() {
+        // non-argument constructor for JPA
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters & setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User [email=" + email + ", id=" + id + ", name=" + name + "]";
+    }
+
+}
